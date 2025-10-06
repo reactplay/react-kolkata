4bc016f709f05cb96185a15ed7df6499c448f137
badaljain <badal.jain77@gmail.com>
2025-10-07 01:06:07 +0530

    fix: build issue
    Signed-off-by: badaljain <badal.jain77@gmail.com>

--- src/app/[locale]/contributors/page.tsx
+++ src/app/[locale]/contributors/page.tsx
@@ -3,10 +3,11 @@ import ContributorsSection from "@/modules/contributors";
 import { getTranslations } from "next-intl/server";
 
 export async function generateMetadata({
-  params: { locale },
+  params,
 }: {
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }): Promise<Metadata> {
+  const { locale } = await params;
   const t = await getTranslations({ locale, namespace: "Contributors" });
 
   return {
