1b35819262e1aa9d75e859529e7b699730f4a71b
Badal Jain <badal.jain77@gmail.com>
2025-10-07 21:59:37 +0530

    fix: update the favicon to use the app logo (#58)

--- src/modules/home/meta/site.ts
+++ src/modules/home/meta/site.ts
@@ -48,8 +48,8 @@ export const siteConfig: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
+    icon: "/logo.svg",
+    shortcut: "/logo.svg",
+    apple: "/logo.svg",
   },
 };
