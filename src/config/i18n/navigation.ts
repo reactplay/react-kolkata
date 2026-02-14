9a4739008a9050bad1f858f09c5f8a63bde9918e
Neeha Agrawal <neehaj12@gmail.com>
2026-02-14 07:56:13 -0500

    feat(common): add spanish language support (#150)
    add spanish translation file and register es locale
    to extend internationalization support beyond
    english, bengali and hindi

--- src/config/i18n/navigation.ts
+++ src/config/i18n/navigation.ts
@@ -2,7 +2,7 @@ import { createNavigation } from "next-intl/navigation";
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  locales: ["en", "bn", "hi"],
+  locales: ["en", "bn", "hi", "es"],
 
   defaultLocale: "en",
 });
